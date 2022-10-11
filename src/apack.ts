747b1a85974f8db56f3bcb6562e5c234474146a5
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2022-10-11 11:52:09 -0400

    Start adding apack json reading code.

--- src/apack.ts
+++ src/apack.ts
@@ -11,23 +11,4 @@
  *
  ******************************************************************************/
 
-// Main extension entry point.
-import * as vscode from 'vscode';
-
-export function activate(context: vscode.ExtensionContext) {
-	
-	vscode.window.showInformationMessage('Extension "zap" is now active!');
-
-	let simpleMessage = vscode.commands.registerCommand('zap.test', () => {
-		vscode.window.showInformationMessage('ZAP Integration!');
-	});
-
-	let showZapUi = vscode.commands.registerCommand('zap.show', () => {
-		vscode.commands.executeCommand('simpleBrowser.show', 'http://localhost:9070')
-	})
-
-	context.subscriptions.push(simpleMessage);
-	context.subscriptions.push(showZapUi);
-}
-
-export function deactivate() {}
+// The purpose of this module is to provide apack.json integration.
